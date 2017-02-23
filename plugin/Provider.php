3ab4b6a09648b0fc10e4f6e95bd0fe183d529855
Sven Speckmaier <sps@ipunkt.biz>
2017-02-23 14:31:09 +0100

    added composer.json and dummy provider

---
+++ plugin/Provider.php
@@ -0,0 +1,16 @@
+<?php
+
+use Rancherize\Plugin\ProviderTrait;
+
+class Provider implements Rancherize\Plugin\Provider {
+
+	use ProviderTrait;
+
+	public function register() {
+		echo "Backup registered!";
+	}
+
+	public function boot() {
+		echo "Booted!";
+	}
+}
